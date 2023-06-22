6e0c20593026624d9dddbaf73273e2641ba7fad9
ualmeida55 <ualmeida55@gmail.com>
2023-06-22 17:54:15 -0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import "./App.css";
+import Task from "./Components/Task";
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    const form = e.target.newItem.value;
+
+    const newTask = {
+      id: Date.now(),
+      description: form,
+    };
+
+    setTasks(prevTasks => [...prevTasks, newTask]);
+
+    e.target.newItem.value = "";
+  };
+
+  const handleDelete = taskId => {
+    setTasks(prevTasks => prevTasks.filter(task => task.id !== taskId));
+  };
+
+  return (
+    <>
+      <h1>Todo List App</h1>
+      <form onSubmit={handleSubmit} action="">
+        <label htmlFor="">
+          New Item
+          <input name="newItem" type="text" />
+        </label>
+        <button type="submit">Add</button>
+      </form>
+      <hr />
+      <Task tasks={tasks} onDelete={handleDelete} />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Task.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+
+function Task({ tasks, onDelete }) {
+  const [completedTasks, setCompletedTasks] = useState([]);
+
+  const handleToggleCompletion = taskId => {
+    setCompletedTasks(prevCompletedTasks => {
+      if (completedTasks.includes(taskId)) {
+        return prevCompletedTasks.filter(id => id !== taskId);
+      } else {
+        return [...prevCompletedTasks, taskId];
+      }
+    });
+  };
+
+  return (
+    <>
+      {tasks.map(task => (
+        <div key={task.id}>
+          <input
+            type="checkbox"
+            onClick={() => handleToggleCompletion(task.id)}
+          ></input>
+          <span
+            style={{
+              textDecoration: completedTasks.includes(task.id)
+                ? "line-through"
+                : "none",
+            }}
+          >
+            {task.description}
+          </span>
+          <button type="submit" onClick={() => onDelete(task.id)}>
+            Delete
+          </button>
+        </div>
+      ))}
+    </>
+  );
+}
+
+export default Task;
